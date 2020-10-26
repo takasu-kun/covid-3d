c083d0228a8fc87884da7ff72a70e9b1657ed61e
Danielle Guerzon <44608503+takasu-kun@users.noreply.github.com>
2020-10-26 11:24:19 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# COVID-3D
+
+3D visualization of latest data regarding Coronavirus cases, provided by the World Health Organization. Available in API form via https://github.com/NovelCOVID/API.
+
+Plotted using [globe.gl](https://github.com/vasturiano/globe.gl).
+
+![demo](https://i.imgur.com/1AFEXhs.png)
+
+## Installing
+
+1. Install dependencies
+
+   ```bash
+   npm install
+   ```
+
+2. Fire up the server and watch files
+
+   ```bash
+   npm run dev
+   ```
+
+## Built with
+
+- [globe.gl](https://github.com/vasturiano/globe.gl)
+- [d3.js](https://d3js.org/)
+- [CountUp.js](https://inorganik.github.io/countUp.js/)
+- https://github.com/NovelCOVID/API
+
+## License & copyright
+
+© Robert C Soriano
+
+Licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- Inspired by https://covidvisualizer.com
+- Hat tip to anyone whos module was used
+# covid-3d
---
+++ src/app.js
@@ -0,0 +1,122 @@
+import Globe from 'globe.gl';
+import { CountUp } from 'countup.js';
+import { request } from './utils';
+import * as d3 from 'd3';
+
+// Globe container
+const globeContainer = document.getElementById('globeViz');
+// Globe image url
+const globeImageUrl =
+  '//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg';
+// Background image url
+const backgroundImageUrl =
+  '//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png';
+// Geojson url
+const geojsonUrl =
+  'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson';
+// COVID-19 cases api
+const apiUrl = 'https://corona.lmao.ninja/v2/countries?sort=country';
+
+const colorScale = d3.scaleSequentialPow(d3.interpolateOrRd).exponent(1/4)
+const getVal = feat => feat.covid.cases;
+
+let world;
+
+init();
+
+function init() {
+  world = Globe()(globeContainer)
+    .globeImageUrl(globeImageUrl)
+    .backgroundImageUrl(backgroundImageUrl)
+    .showGraticules(false)
+    .polygonAltitude(0.06)
+    .polygonCapColor(feat => colorScale(getVal(feat)))
+    .polygonSideColor(() => 'rgba(0, 100, 0, 0.05)')
+    .polygonStrokeColor(() => '#111')
+    .polygonLabel(
+      ({ properties: d, covid: c }) => `
+            <div class="card">
+              <img class="card-img" src="${c.countryInfo.flag}" alt="flag" />
+              <div class="container">
+                 <span class="card-title"><b>${d.ADMIN}</b></span> <br />
+                 <span class="card-total-cases">${c.cases} total cases</span>
+                 <div class="card-spacer"></div>
+                 <hr />
+                 <div class="card-spacer"></div>
+                 <span>${c.active} active</span> <br />
+                 <span>${c.deaths} dead</span> <br />
+                 <span>${c.recovered} recovered</span>
+                 <div class="card-spacer"></div>
+                 <hr />
+                 <div class="card-spacer"></div>
+                 <div class="bottom-info">
+                  <span style="color: goldenrod;">Today</span>
+                  <span>${c.todayCases} cases</span>
+                  <span>${c.todayDeaths} deaths</span>
+                 </div>
+              </div>
+            </div>
+          `
+    )
+    .onPolygonHover(hoverD => world
+      .polygonAltitude(d => d === hoverD ? 0.12 : 0.06)
+      .polygonCapColor(d => d === hoverD ? 'steelblue' : colorScale(getVal(d)))
+    )
+    .polygonsTransitionDuration(300);
+
+  getCases();
+}
+
+async function getCases() {
+  const countries = await request(geojsonUrl);
+  const data = await request(apiUrl);
+
+  const countriesWithCovid = []
+
+  data.forEach(item => {
+    const countryIdx = countries.features.findIndex(
+      i => i.properties.ISO_A2 === item.countryInfo.iso2 && i.properties.ISO_A3 === item.countryInfo.iso3
+    );
+
+    if (countryIdx !== -1) {
+      countriesWithCovid.push({
+        ...countries.features[countryIdx],
+        covid: item
+      });
+    }
+
+    
+    const maxVal = Math.max(...countriesWithCovid.map(getVal));
+    colorScale.domain([0, maxVal]);
+  });
+
+  world.polygonsData(countriesWithCovid);
+  document.querySelector('.title-desc').innerHTML = 'Hover on a country or territory to see cases, deaths, and recoveries.'
+
+  // Show total counts
+  const totalInfected = data.reduce((a, b) => a + b.cases, 0);
+  const infected = new CountUp('infected', totalInfected);
+  infected.start();
+
+  const totalDeaths = data.reduce((a, b) => a + b.deaths, 0);
+  const deaths = new CountUp('deaths', totalDeaths);
+  deaths.start();
+
+  const totalRecovered = data.reduce((a, b) => a + b.recovered, 0);
+  const recovered = new CountUp('recovered', totalRecovered);
+  recovered.start();
+
+  // Get IP Address
+  const { latitude, longitude } = await request('https://geolocation-db.com/json/');
+
+  world.pointOfView({
+    lat: latitude,
+    lng: longitude
+  }, 2000);
+}
+
+// Responsive globe
+window.addEventListener('resize', (event) => {
+  world.width([event.target.innerWidth])
+  world.height([event.target.innerHeight])
+});
